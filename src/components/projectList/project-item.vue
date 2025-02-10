46ae1931c877bd507bcc51187895989733c4f0b0
zhijiezheng <zhijiezheng@tencent.com>
2025-02-10 23:22:28 +0800

    new

--- src/components/projectList/project-item.vue
+++ src/components/projectList/project-item.vue
@@ -15,7 +15,7 @@
         <!--        </div>-->
 
         <div v-if="project.detail" class="row">
-          <div class="text-weight-bold text-subtitle2 col-auto">岗位介绍</div>
+          <div class="text-weight-bold text-subtitle2 col-auto">岗位介绍：</div>
           <div class="col">{{ project.detail }}</div>
         </div>
 
